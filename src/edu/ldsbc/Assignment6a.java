4f3171a384997d4f1bdbe36bc4bf794354488bf2
Craig <craigsmith2005@aim.com>
2017-10-15 15:14:27 -0600

    chapter 6

---
+++ src/edu/ldsbc/Assignment6a.java
@@ -0,0 +1,30 @@
+package edu.ldsbc;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Assignment6a {
+    /**
+     CS 114 - Assignment 6a
+     Package Name: chapter6
+     Objective: Methods
+     Import: java.util.scanner
+
+
+     Write a method named isDivisible that takes two integers, n and m, and that returns true if n is divisible by m, and false otherwise.
+
+     Example:
+
+     Enter n, m: 7 8
+     false
+
+     Enter n,m: 22 11
+     true
+
+     */
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/edu/ldsbc/Assignment6b.java
@@ -0,0 +1,28 @@
+package edu.ldsbc;
+
+public class Assignment6b {
+    /**
+     Package Name: chapter6
+     Objective: Methods
+     Import: java.util.scanner
+     Read the instructions carefully.  Make sure you are outputting things as per the example.
+     Remember to comment your code more as explained in class.
+
+     Write a method that receives as parameters three numbers.  Return the largest number.
+     Name the method “maxNum”.
+     Example:
+
+     Enter three numbers: 23 65 2
+     Largest number is 65
+
+
+
+
+     */
+
+
+    public static void main(String[] args) {
+
+        
+    }
+}
