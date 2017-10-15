1acee3782a39d989737cdaad0c1456036bf5f00f
Craig <craigsmith2005@aim.com>
2017-10-15 15:40:05 -0600

    chapter 6

--- src/edu/ldsbc/Assignment6a.java
+++ src/edu/ldsbc/Assignment6a.java
@@ -4,27 +4,34 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Assignment6a {
     /**
-     CS 114 - Assignment 6a
-     Package Name: chapter6
-     Objective: Methods
-     Import: java.util.scanner
-
-
-     Write a method named isDivisible that takes two integers, n and m, and that returns true if n is divisible by m, and false otherwise.
-
-     Example:
-
-     Enter n, m: 7 8
-     false
-
-     Enter n,m: 22 11
-     true
-
+     * CS 114 - Assignment 6a
+     * Package Name: chapter6
+     * Objective: Methods
+     * Import: java.util.scanner
+     * <p>
+     * <p>
+     * Write a method named isDivisible that takes two integers,
+     * n and m, and that returns true if n is divisible by m, and false otherwise.
+     * <p>
+     * Example:
+     * <p>
+     * Enter n, m: 7 8
+     * false
+     * <p>
+     * Enter n,m: 22 11
+     * true
      */
 
 
     public static void main(String[] args) {
+        int m = 10;
+        int n = 20;
 
+        boolean result = isDivisible(m, n);
+        System.out.println(result);
+    }
 
+    private static boolean isDivisible(int m, int n) {
+        return n % m == 0;
     }
 }
--- src/edu/ldsbc/Assignment6b.java
+++ src/edu/ldsbc/Assignment6b.java
@@ -2,27 +2,43 @@ package edu.ldsbc;
 
 public class Assignment6b {
     /**
-     Package Name: chapter6
-     Objective: Methods
-     Import: java.util.scanner
-     Read the instructions carefully.  Make sure you are outputting things as per the example.
-     Remember to comment your code more as explained in class.
-
-     Write a method that receives as parameters three numbers.  Return the largest number.
-     Name the method “maxNum”.
-     Example:
-
-     Enter three numbers: 23 65 2
-     Largest number is 65
-
-
-
-
+     * Package Name: chapter6
+     * Objective: Methods
+     * Import: java.util.scanner
+     * Read the instructions carefully.  Make sure you are outputting things as per the example.
+     * Remember to comment your code more as explained in class.
+     * <p>
+     * Write a method that receives as parameters three numbers.  Return the largest number.
+     * Name the method “maxNum”.
+     * Example:
+     * <p>
+     * Enter three numbers: 23 65 2
+     * Largest number is 65
      */
 
 
     public static void main(String[] args) {
 
-        
+        System.out.println(findLargest(1, 3, 2));
+
+    }
+
+    private static int findLargest(int a, int b, int c) {
+        int largestNum = findLargest(a, b);
+        largestNum = findLargest(largestNum, c);
+        return largestNum;
+    }
+
+    private static int findLargest(int a, int b) {
+        int retValue;
+        if (a > b) {
+            retValue = a;
+        } else {
+            retValue = b;
+        }
+        //ternary operator
+        //retValue = (a < b) ? i : i1;
+
+        return retValue;
     }
 }
