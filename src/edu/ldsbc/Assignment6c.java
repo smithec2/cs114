b879759354bae9f9fc92058ba549e02d3d01e121
Craig <craigsmith2005@aim.com>
2017-10-15 15:05:09 -0600

    chapter 5

---
+++ src/edu/ldsbc/Assignment6c.java
@@ -0,0 +1,31 @@
+package edu.ldsbc;
+
+public class Assignment6c {
+    /**
+     Package Name: chapter6
+     Objective: Methods, Recursion
+     Import: java.util.scanner
+     Read the instructions carefully.  Make sure you are outputting things as per the example.
+     Remember to comment your code more as explained in class.
+
+     Write a recursive method called power that takes a double x and an integer n and returns xn.
+     Hint: A recursive definition of this operation is xn = x * xn-1. Also, remember that anything raised to the zeroth power is 1.
+     Example:
+
+     Enter x and n: 4.5  3
+     91.125
+
+
+
+
+
+
+
+     */
+
+
+    public static void main(String[] args) {
+
+        
+    }
+}
