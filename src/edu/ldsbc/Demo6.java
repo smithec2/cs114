ee8f54a785d8149c162e2a7dc949cd857ae25960
smirhec2 <gobionx@mail.com>
2017-10-15 19:25:13 -0600

    chapter 6

---
+++ src/edu/ldsbc/Demo6.java
@@ -0,0 +1,21 @@
+package edu.ldsbc;
+
+public class Demo6 {
+    /**
+     * @author Craig Smith
+     * 10/14/2017
+     *f1
+     * f2
+     * times table
+     */
+
+    public static void main(String[] args) {
+
+        f1();
+        
+    }
+
+    private static void f1() {
+        System.out.println("in f1");
+    }
+}
