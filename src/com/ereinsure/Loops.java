c710480670d66a043ca75467a1eff79d7e68a397
smirhec2 <gobionx@mail.com>
2017-10-23 16:15:29 -0600

    Added a comment

--- src/com/ereinsure/Loops.java
+++ src/com/ereinsure/Loops.java
@@ -3,17 +3,21 @@ package com.ereinsure;
 import java.util.*;
 import java.util.function.Consumer;
 
+/**
+ *
+ */
 public class Loops {
     public static void main(String[] args) {
 
-        //while
-        boolean sentnalValue = true;
-        while(sentnalValue)
+        //while (brake)
+        //      int count = 0;   // count how many divisions we've done
+        boolean sentinelValue = true;
+        while(sentinelValue)
         {
-
+            System.out.println();
         }
 
-        //for
+        //for (start at 10 to 20), (i+=2), (count backwards)
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
