dda65d5e44f9cccba5328d262cde3083569940e9
Craig <craigsmith2005@aim.com>
2017-10-29 22:35:20 -0600

    chapter 8

---
+++ src/edu/ldsbc/ArraysDemo.java
@@ -0,0 +1,32 @@
+package edu.ldsbc;
+
+import java.util.Arrays;
+
+public class ArraysDemo {
+    /**
+     * @author Craig Smith
+     * 10/28/2017
+     */
+
+    public static void main(String[] args) {
+
+        String[] a = {"Aspen", "Scott"};
+        String[] b = new String[2];
+        // read
+        //write
+
+        int[] c = {1, 2, 3};
+        int[] d = Arrays.copyOf(c, 2); //length
+
+        //loop (length)
+        //for
+        //for each
+
+        // indexOfMax (homework)
+
+
+        //list
+        //map
+
+    }
+}
